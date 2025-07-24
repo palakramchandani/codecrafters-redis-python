435e82b4a528ffa46059d6adde015e02e9f537e5
Palak Ramchandani <palakramchandani048@gmail.com>
2025-07-24 19:24:57 +0530

    [any message]

--- app/main.py
+++ app/main.py
@@ -1,19 +1,24 @@
 import socket  # noqa: F401
 
 
-def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
+def handle_client(connection,address):
 
-    # Uncomment this to pass the first stage
-    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    connection, _ = server_socket.accept() 
     while True:
         data = connection.recv(1024)
         if not data:
             break
         connection.sendall(b"+PONG\r\n")
-    
+def main():
+    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
+    server_socket.listen()
+    print("Server is listening on port 6379")
+
+while True:
+        connection, address = server_socket.accept()
+        print(f"Accepted connection from {address}")
+        thread= threading.Thread(target=handle_client, args=(connection, address))
+        thread.daemon = True
+        thread.start()  
 
 
 if __name__ == "__main__":
